f3a521ecfe3f5ac936cbe0ba8563ae68fa653b1b
GueryMathieu <hamta15300@gmail.com>
2019-06-10 12:22:19 +0200

    ILBOUGE

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
-var Int = setInterval('moveAliens()', 1000);
+var largeur = screen.availWidth
+var hauteur = screen.availHeight
+var vaisseau = (largeur/2)-110
 
-var screen = ('screen');
-var alien = ('alien');
-var posXalien = alien.height();
-var posYalien = alien.width();
-var posXscreen = screen.innerHeight();
-var posYscreen = screen.innerWidth();
+function redim(){
 
-function moveAliens() {
-
-    if (posXalien < posXscreen) {
-        posXalien = posXalien+1;
-    }
-
-    else {
-        posXalien = posXalien-1;
-    }
+    document.bgColor="black"
+    document.getElementById('space').style.left=(largeur/2)-400+'px'
+    document.getElementById('space').style.top=(hauteur/2)-300+'px'
+    document.getElementById('spaceship').style.top=(hauteur/2)+72+'px'
+    document.getElementById('spaceship').style.left=(largeur/2)-110+'px'
 
 };
 
-clearInterval(Int);
+function spaceshipMoves(keyCode)
+{
+    switch(keyCode)
+    {
+    case 37: bouge(0,-15); break;
+    case 39: bouge(15,0); break;
+
+    }
+}
+
+function bouge(x,y)
+{
+    min=(largeur/2)-420
+    max=(largeur/2)-400+620
+
+
+    if(vaisseau<max)
+    {
+        vaisseau+=x
+
+        document.getElementById('spaceship').style.left=vaisseau +'px'
+
+    }
+
+    if(vaisseau>min)
+    {
+        vaisseau+=y
+
+        document.getElementById('spaceship').style.left=vaisseau +'px'
+
+    }
+}
